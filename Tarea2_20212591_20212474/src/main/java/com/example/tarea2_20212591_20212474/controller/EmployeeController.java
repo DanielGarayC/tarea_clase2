38a1732f0d2a211b3c160c2cc906c124e28b36f8
Daniel <a20212591@pucp.edu.pe>
2024-09-07 02:35:23 -0500

    controller

--- Tarea2_20212591_20212474/src/main/java/com/example/tarea2_20212591_20212474/controller/EmployeeController.java
+++ Tarea2_20212591_20212474/src/main/java/com/example/tarea2_20212591_20212474/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.example.tarea2_20212591_20212474.controller;
 import com.example.tarea2_20212591_20212474.repository.EmployeeRepository;
 import com.example.tarea2_20212591_20212474.repository.JobRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeController {
@@ -10,5 +12,24 @@ public class EmployeeController {
 
     public EmployeeController(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
+    }
+    @GetMapping({"employee/list", "employee"})
+    public String listarEmpleados() {
+//        COMPLETAR
+        return "XXXXXX";
+    }
+
+    @GetMapping("")
+    public String informEmpleado() {
+        //        COMPLETAR
+        return "XXXXXX";
+    }
+
+    @GetMapping("")
+    public String borrarEmpleado(Model model) {
+        model.addAttribute("tipo_alert", "success");
+        model.addAttribute("msg", "Se borro el empleado");
+//        COMPLETAR
+        return "XXXXXX";
     }
 }
